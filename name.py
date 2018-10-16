9c1bb87cfe7764de8972f57a22404416b5e1e90e
S-Lam <sla@pfb15.cshl.edu>
2018-10-16 11:33:22 -0400

    updated name.py

--- name.py
+++ name.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
+import sys
+
+myName = sys.argv[1]
+myFavColor = sys.argv[2]
+myFavActivity = sys.argv[3]
+myFavAnimal = sys.argv[4]
+
+print ("My name is", myName, "and my favorite color is", myFavColor + ". My favorite activity is", myFavActivity, "and my favorite animal is", myFavAnimal + ".")
+
+
 print ("My name: Stephanie")
+
+print ("My favorite color: blue")
+
+favActivity = "eating"
+
+print ("My favorite activity:", favActivity)
+
+print ("My favorite animal: manatee")
